aec376bf51cc2c59a9012d856c392c913848176c
Leonardo Maia <86787664+leomaiaao@users.noreply.github.com>
2022-06-21 20:27:25 -0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  API_USER: 'http://localhost:3000/Users',
+  API_USER: 'https://leo-codeflix.herokuapp.com/Users',
   API_KEY: 'f4da42263cee6bb7b4d913ab8fbae012',
   API_MOVIES_DISCOVER:'https://api.themoviedb.org/3/discover/movie',
   API_MOVIES:'https://api.themoviedb.org/3/movie'
