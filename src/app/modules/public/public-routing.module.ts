63857d64f4a143b3b6f4190ada8f39095e01877d
Leonardo Maia <86787664+leomaiaao@users.noreply.github.com>
2022-06-26 17:09:48 -0300

    Update public-routing.module.ts

--- src/app/modules/public/public-routing.module.ts
+++ src/app/modules/public/public-routing.module.ts
@@ -3,13 +3,16 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { LoginCardComponent } from './components/login-card/login-card.component';
 import { SignUpCardComponent } from './components/sign-up-card/sign-up-card.component';
+import { PesquisaComponent } from './components/pesquisa/pesquisa.component';
+
 
 const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: 'home', component: HomeComponent},
   {path: 'login', component: LoginCardComponent},
   {path: 'signUp', component: SignUpCardComponent},
-  {path: '**', redirectTo: 'home', pathMatch: 'full'}
+  {path: 'pesquisa', component: PesquisaComponent}
+
 ];
 
 @NgModule({
